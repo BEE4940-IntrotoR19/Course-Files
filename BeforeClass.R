c4b01fe62da1dd8043af7d8ed50944ca0b36f31a
Erin Menzies Pluer <46326698+EGMP@users.noreply.github.com>
2019-01-03 13:58:46 -0500

    Add files via upload

---
+++ BeforeClass.R
@@ -0,0 +1,25 @@
+#Hey there! This script will get you started getting comfortable in R
+#To execute lines of code in the script hit CTRL+Enter or CTRL+R if you have an older version
+#Let's play around a bit
+  #Can you: 
+    #do some basic math?
+          12+3
+    #save a variable?
+          x=6 
+          y=5
+  	#What's: 
+        x+y 
+        z=x+y
+
+#Have you figured out the difference between the console and the script?
+        
+        
+#What about the environment? What's that? 
+        
+        
+#Create a new blank script and save it as "R Practice" in the folder you created for this class
+#Play around to figure out how to do this, explore the toolbar at the top
+
+#To write a comment in your script type #and then type. The text will turn green.    
+#Comment your answers to the above questions and play around a bit more in your "R Practice.R" script 
+
